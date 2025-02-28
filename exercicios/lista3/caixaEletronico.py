06bd37ba1adca2c7b12848fee07541a3b4a3d1c5
Elian-BSiqueira <elian.b.siqueira@gmail.com>
2025-02-28 17:06:25 -0300

    Programa caixaEletronico feito usando if, elif e while

--- exercicios/lista3/caixaEletronico.py
+++ exercicios/lista3/caixaEletronico.py
@@ -1,17 +1,45 @@
 while True:
     print("Valor minimo de saque: 10 reais\n"
           "Valor maximo de saque: 600 reais")
-    numero = int(input("Digite o valor a ser sacado: "))
+    valor = int(input("Digite o valor a ser sacado: "))
 
-    if 10 <= numero <= 600:
+    if 10 <= valor <= 600:
         break
 
     else:
         print("Valor invalido")
         continue
 
-centenas = numero // 100
-dezenas = (numero % 100) // 10
-unidades = numero % 10
+total = valor
+ced = 100
+totalced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totalced += 1
 
-partes = []
+    else:
+        if totalced > 0:
+            print(f"Total de {totalced} cedulas de R${ced}")
+        if ced == 100:
+            ced = 50
+            totalced = 0
+
+        elif ced == 50:
+            ced = 20
+            totalced = 0
+
+        elif ced == 20:
+            ced = 10
+            totalced = 0
+
+        elif ced == 10:
+            ced = 5
+            totalced = 0
+
+        elif ced == 5:
+            ced = 1
+            totalced = 0
+
+        if total == 0:
+            break
