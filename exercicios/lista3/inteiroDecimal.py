ffd8ec7001d6de3b3803e600226a7c4f5a965646
Elian-BSiqueira <elian.b.siqueira@gmail.com>
2025-03-03 13:04:15 -0300

    update inteiroDecimal
    Programa feito de outra forma

--- exercicios/lista3/inteiroDecimal.py
+++ exercicios/lista3/inteiroDecimal.py
@@ -1,15 +1,20 @@
-numero = float(input("Digite um numero: "))
-inteiroOuDecimal = round(numero, 0) == numero
 
-if numero > 0:
-    sinal = "positivo"
+while True:
+    try:
+        numero = float(input("Digite um numero: "))
+        break
+
+    except ValueError:
+        print("Digite apenas numeros")
+
+valor = ""
+
+
+
+if int(numero) == numero:
+    valor = "inteiro"
 
 else:
-    sinal = "negativo"
+    valor = "decimal"
 
-
-if inteiroOuDecimal:
-    print(f"O numero {numero} e inteiro e e {sinal}")
-
-else:
-    print(f"O numero {numero} e decimal e e {sinal}")
+print(valor)
