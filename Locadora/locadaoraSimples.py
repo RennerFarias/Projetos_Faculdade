590b1523caa1fe94672bccbef4203e15359e0b44
Renner <rennerbarbosadefarias76@gmail.com>
2025-03-06 18:29:43 -0300

    Update locadaoraSimples.py

--- Locadora/locadaoraSimples.py
+++ Locadora/locadaoraSimples.py
@@ -1,71 +1,93 @@
-from time import sleep
+import time
 
-filmes = ['Os Banshees de Inisherin', 'Barbie', 'John Wick 4: Baba Yaga', 'Creed III', 'Top Gun: Maverick',
-          'Assassinos da Lua das Flores', 'Close', 'Sorria', 'Homem-Aranha: Através do Aranhaverso', 'Oppenheimer']
+filmes = ['Oppenheimer' , 'Doctor Who' , 'Corra']
+filmes_alugados = []
 
-opcaoEscolhida = None
-while opcaoEscolhida != 5:
-    print('=' * 30)
-    print('Locadora'.center(28))
-    print('=' * 30)
-    print('Opcao 1 - Listar filmes')
-    print('Opcao 2 - Alugar filme')
-    print('Opcao 3 - Devolver filme')
-    print('Opcao 4 - Adicionar filme')
-    print('Opcao 5 - Sair')
-
-    opcaoEscolhida = input('Digite sua opcao: ')
-    sleep(0.5)
-
-    if opcaoEscolhida.isdigit():
-        opcaoEscolhida = int(opcaoEscolhida)
+def listar_filmes():
+    if filmes:
+        print('Filmes disponíveis:')
+        for i, filme in enumerate(filmes, start=1):
+            print(f'{i} - {filme}')
+            time.sleep(0.3)
     else:
-        print('\033[31mERRO. DIGITE APENAS NUMEROS\033[m')
-        sleep(0.5)
-        continue
+        print('Nenhum filme disponível no momento.')
 
-    if opcaoEscolhida == 1:
-        if filmes:
-            print('Filmes disponíveis:')
-            for i, filme in enumerate(filmes, start=1):
-                print(f'{i} - {filme}')
-                sleep(0.3)
-        else:
-            print('Nenhum filme disponível no momento.')
-
-    elif opcaoEscolhida == 2:
-        if not filmes:
-            print('Nenhum filme disponível para alugar.')
-        else:
-            print('Filmes disponíveis:')
-            for i, filme in enumerate(filmes, start=1):
-                print(f'{i} - {filme}')
-                sleep(0.3)
-            numeroDoFilmeEscolhido = input('Digite o número do filme que deseja alugar: ')
-            if numeroDoFilmeEscolhido.isdigit():
-                numeroDoFilmeEscolhido = int(numeroDoFilmeEscolhido)
-                if 0 < numeroDoFilmeEscolhido <= len(filmes):
-                    filmeAlugado = filmes.pop(numeroDoFilmeEscolhido - 1)
-                    print(f'Filme {filmeAlugado} alugado com sucesso')
-                else:
-                    print('\033[31mNúmero inválido. Escolha um filme da lista.\033[m')
+def alugar_filme():
+    if not filmes:
+        print('Nenhum filme disponível para alugar.')
+    else:
+        listar_filmes()
+        numero = input('Digite o número do filme que deseja alugar: ')
+        if numero.isdigit():
+            numero = int(numero)
+            if 1 <= numero <= len(filmes):
+                filme = filmes.pop(numero - 1)
+                filmes_alugados.append(filme)
+                print(f'Filme {filme} alugado com sucesso')
             else:
-                print('\033[31mERRO. DIGITE APENAS NUMEROS\033[m')
-        sleep(0.3)
+                print('Número inválido. Escolha um filme da lista.')
+        else:
+            print('ERRO. DIGITE APENAS NÚMEROS')
 
-    elif opcaoEscolhida == 3:
-        filmeDevolvido = input('Digite o nome do filme devolvido: ').strip().title()
-        filmes.append(filmeDevolvido)
-        print(f'Filme {filmeDevolvido} devolvido com sucesso')
-
-    elif opcaoEscolhida == 4:
-        adicionarFilme = input('Digite o nome do filme que deseja adicionar: ').strip().title()
-        filmes.append(adicionarFilme)
-        print(f'Filme {adicionarFilme} adicionado com sucesso')
-
-    elif opcaoEscolhida == 5:
-        print('Programa encerrado com sucesso')
-
+def devolver_filme():
+    if not filmes_alugados:
+        print('Nenhum filme alugado para devolver.')
+        return
+    
+    print('Filmes alugados:')
+    for i, filme in enumerate(filmes_alugados, start=1):
+        print(f'{i} - {filme}')
+    
+    numero = input('Digite o número do filme que deseja devolver: ')
+    if numero.isdigit():
+        numero = int(numero)
+        if 1 <= numero <= len(filmes_alugados):
+            filme = filmes_alugados.pop(numero - 1)
+            filmes.append(filme)
+            print(f'Filme {filme} devolvido com sucesso')
+        else:
+            print('Número inválido. Escolha um filme da lista de filmes alugados.')
     else:
-        print('\033[31mOpcao invalida! Escolha entre 1 e 5.\033[m')
-        sleep(1)
+        print('ERRO. DIGITE APENAS NÚMEROS')
+
+
+def adicionar_filme():
+    filme = input('Digite o nome do filme que deseja adicionar: ').strip().title()
+    filmes.append(filme)
+    print(f'Filme {filme} adicionado com sucesso')
+
+def menu():
+    while True:
+        print('=' * 30)
+        print('Locadora'.center(30))
+        print('=' * 30)
+        print('1 - Listar filmes')
+        print('2 - Alugar filme')
+        print('3 - Devolver filme')
+        print('4 - Adicionar filme')
+        print('5 - Sair')
+
+        opcao = input('Digite sua opção: ')
+        time.sleep(0.5)
+
+        if opcao.isdigit():
+            opcao = int(opcao)
+            if opcao == 1:
+                listar_filmes()
+            elif opcao == 2:
+                alugar_filme()
+            elif opcao == 3:
+                devolver_filme()
+            elif opcao == 4:
+                adicionar_filme()
+            elif opcao == 5:
+                print('Programa encerrado com sucesso')
+                break
+            else:
+                print('Opção inválida! Escolha entre 1 e 5.')
+        else:
+            print('ERRO. DIGITE APENAS NÚMEROS')
+        time.sleep(1)
+
+if __name__ == '__main__':
+    menu()
